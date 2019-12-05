bc7815c14996400012d1bb1a116d7a56388c75d5
flifloo <flifloo@gmail.com>
2019-12-06 00:38:38 +0100

    Setup models

--- app/models.py
+++ app/models.py
@@ -3,8 +3,140 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    university_id = db.Column(db.Integer, db.ForeignKey("university.id"))
+
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String)
+    email = db.Column(db.String)
+
+    housing = db.relationship("Housing", backref="user", lazy="dynamic")
+    study = db.relationship("Study", backref="user", lazy="dynamic")
+    technology = db.relationship("Technology", backref="user", lazy="dynamic")
+    finance = db.relationship("Finance", backref="user", lazy="dynamic")
+    handicap = db.relationship("Handicap", backref="user", lazy="dynamic")
+    family = db.relationship("Family", backref="user", lazy="dynamic")
 
     def __repr__(self):
-        return f"<Users {self.username}>"
+        return f"<User {self.username}>"
+
+
+class Housing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    typeHousing_id = db.Column(db.Integer, db.ForeignKey("type_housing.id"))
+    typeTransport_id = db.Column(db.Integer, db.ForeignKey("type_transport.id"))
+
+    price = db.Column(db.Integer)
+    wholesomeness = db.Column(db.Boolean)
+    transportTime = db.Column(db.Integer)
+    distance = db.Column(db.Integer)
+    needs_proximity = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Housing {self.id}>"
+
+
+class TypeHousing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
+
+    housing = db.relationship("Housing", backref="type_housing", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<TypeHousing {self.id}>"
+
+
+class TypeTransport(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
+
+    housing = db.relationship("Housing", backref="type_transport", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<TypeTransport {self.id}>"
+
+
+class Study(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    formation_id = db.Column(db.Integer, db.ForeignKey("formation.id"))
+
+    level = db.Column(db.Integer)
+    douille = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Study {self.id}>"
+
+
+class Formation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
+
+    study = db.relationship("Study", backref="formation", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<Formation {self.id}>"
+
+
+class University(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    location = db.Column(db.String)
+
+    user = db.relationship("User", backref="university", lazy="dynamic")
+
+    def __repr__(self):
+        return f"<University {self.id}>"
+
+
+class Technology(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    hardware = db.Column(db.Boolean)
+    internet = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f"<Technology {self.id}>"
+
+
+class Finance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    scholarships = db.Column(db.Boolean)
+    APL = db.Column(db.Boolean)
+    other = db.Column(db.Integer)
+    family = db.Column(db.Integer)
+    work = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Finance {self.id}>"
+
+
+class Handicap(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    physical = db.Column(db.Boolean)
+    mental = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f"<Handicap {self.id}>"
+
+
+class Family(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    death = db.Column(db.Boolean)
+    divorce = db.Column(db.Boolean)
+    distance = db.Column(db.Integer)
+    violence = db.Column(db.Boolean)
+    
+    def __repr__(self):
+        return f"<Family {self.id}>"
--- app/schemas.py
+++ app/schemas.py
@@ -5,3 +5,53 @@ from app.models import *
 class UsersSchema(ModelSchema):
     class Meta:
         model = User
+
+
+class HousingSchema(ModelSchema):
+    class Meta:
+        model = Housing
+
+
+class TypeHousingSchema(ModelSchema):
+    class Meta:
+        model = TypeHousing
+
+
+class TypeTransportSchema(ModelSchema):
+    class Meta:
+        model = TypeTransport
+
+
+class StudySchema(ModelSchema):
+    class Meta:
+        model = Study
+
+
+class FormationSchema(ModelSchema):
+    class Meta:
+        model = Formation
+
+
+class UniversitySchema(ModelSchema):
+    class Meta:
+        model = University
+
+
+class TechnologySchema(ModelSchema):
+    class Meta:
+        model = Technology
+
+
+class FinanceSchema(ModelSchema):
+    class Meta:
+        model = Finance
+
+
+class HandicapSchema(ModelSchema):
+    class Meta:
+        model = Handicap
+
+
+class FamilySchema(ModelSchema):
+    class Meta:
+        model = Family
