839f8703bf9c8525baa6d042e24b0af4192cf8dd
flifloo <flifloo@gmail.com>
2019-12-06 03:07:16 +0100

    MONEY MONEY HOLY FUCK

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ pip3 install -r requirement.txt
 python3.7 -m flask db init
 python3.7 -m flask db migrate
 python3.7 -m flask db upgrade
-nohup python3.7 -m flask run $
+nohup python3.7 -m flask run &
